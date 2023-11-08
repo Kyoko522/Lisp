86710152105abaa64f1ff559ed25cc857f749b8b
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-11-08 13:00:49 -0500

    Lab 6 added

---
+++ Labs/Lab_6/q2.lisp
@@ -0,0 +1,24 @@
+(defun interleave (listA listB &optional (result '()))
+  (cond
+    ((and (null listA) (null listB))
+     (nreverse result))
+    ((null listA)
+     (interleave listA (cdr listB) (cons (car listB) result)))
+    ((null listB)
+     (interleave (cdr listA) listB (cons (car listA) result)))
+    (t
+     (interleave (cdr listA) (cdr listB) (cons (car listB) (cons (car listA) result)))))
+  )
+
+
+(defun comb (n k)
+  (cond
+    ((or (zerop n) (zerop k)) 1)
+    ((= n k) 1)
+    (t (+ (comb (- n 1) k) (comb (- n 1) (- k 1))))))
+
+
+(defun mapf (f a &optional (result '()))
+  (if (null a)
+      (nreverse result)
+      (mapf f (cdr a) (cons (funcall f (car a)) result))))
